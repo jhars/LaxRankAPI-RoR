1a906fbc7fcb483a24a4f140c3e6a380482cd9ac
jhars <jjharlan06@gmail.com>
2016-02-07 21:59:04 -0800

    drilling down & saving opp_data Objects

--- app/services/create_team_schedule.rb
+++ app/services/create_team_schedule.rb
@@ -15,71 +15,60 @@ class CreateTeamSchedule
 # available objects in term.
 		schedule_object = Hash.new
 		opp_data_array = []
-		opponent_array = []
+		opp_name_array = []
+		opp_link_array = []
+		opp_record_array = []
+		opp_league_array = []
+		game_date_array = []
 	# - - - - - - - - - - - - - - #
 		score_array = []
-		link_array = []
 		home_or_away_array = []
 #-------------------------------------#
 # SCORE-SCRAPER
 		parse_page.css('.team_table > tbody > tr > td.score').map do |data|
 			score = data.text
-# must refine score data-structure
 			score_array.push(score)
 		end
 # OPPONENT-SCRAPER
-		parse_page.css('.team_table > tbody > tr > td > a').map do |data|
-			link_array.push(data.attributes["href"].value)
-			opponent_array.push(data.text)
-		end
 		# parse_page.css('.team_table > tbody > tr > td.opponent').map do |data|
 		parse_page.css('.team_table > tbody > tr').map do |data|
-			#Data is 1 Complete (messy) Opponent Team Object
 			opp_data_array.push(data)
-# EXAMPLE Data-Fetch => 'data.attributes["href"].value'
-	#-Access to following Data:
-			# Opp Name
-			# Opp Link
-			# Opp Record
-			# home OR away - Game
+			game_date_array.push(data.children.children[0])
+			opp_name_array.push(data.children.children[2].children[0])
+			opp_link_array.push(data.children.children[2].attributes["href"].value)
 # - - - - - - - - - - - - - - - - #
-	#-1LevelUP
+#-1LevelUP
+			# home OR away - Game
 			# Game Score
 			# Game Date
 			# Opp Power-Rating
 			# Win Or Loss?
+			opp_league_array.push(data.children[5].children)# Opp League - requires clean up
+			opp_record_array.push(data.children.children[3])# Opp Record - requires clean up
 # - - - - - - - - - - - - - - - - #
 		end
 # ================================================== #
-		temp_opponents = opponent_array			
-		temp_team_links = link_array
-		temp_scores = score_array
-		temp_home_away = "home_away_array"
-		temp_results = "result_array"
-		# - - - - - - - - - - - - - - - - - - - - #
-		schedule_object["opp_teams"] = temp_opponents
-		schedule_object["links"] = temp_team_links
-		schedule_object["scores"] = temp_scores
-		schedule_object["home_away"] = temp_home_away
-		schedule_object["result"] = temp_results
+		# schedule_object["scores"] = temp_scores
+		# schedule_object["home_away"] = temp_home_away
+		# schedule_object["result"] = temp_results
 # ================================================== #
 		Pry.start(binding)
 	end
 
-	def save_schedule
-#-------------------------------------#
-			temp_opponents = opponent_array			
-			temp_team_links = link_array
-			temp_scores = "score_array"
-			temp_home_away = "home_away_array"
-			temp_result = "result_array"
-#-------------------------------------#
-			schedule_object["opp_teams"] = temp_opponents
-			schedule_object["links"] = temp_team_link
-			schedule_object["scores"] = temp_scores
-			schedule_object["home_away"] = temp_home_away
-			schedule_object["result"] = result_array
-	end
+# 	def save_schedule
+# #-------------------------------------#
+# 			# temp_opponents = opponent_array			
+# 			# temp_team_links = link_array
+# 			temp_scores = "score_array"
+# 			temp_home_away = "home_away_array"
+# 			temp_result = "result_array"
+# #-------------------------------------#
+# 			# schedule_object["opp_teams"] = temp_opponents
+# 			# schedule_object["links"] = temp_team_link
+# 			schedule_object["scores"] = temp_scores
+# 			schedule_object["home_away"] = temp_home_away
+# 			schedule_object["result"] = result_array
+# 	end
 
 
 end
