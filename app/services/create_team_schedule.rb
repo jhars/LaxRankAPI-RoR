52350af177f77f878ca37f1c5c56271822354971
jhars <jjharlan06@gmail.com>
2016-02-07 20:23:08 -0800

    designed schedule_object Data-Structure

--- app/services/create_team_schedule.rb
+++ app/services/create_team_schedule.rb
@@ -1,41 +1,69 @@
 class CreateTeamSchedule
+	
 	require 'httparty'
 	require 'nokogiri'
 	require 'json'
 	require 'pry'
 	require 'csv'
 
+# variables accessible in terminal -- get 'schedule/index'	
 	def scrape_schedule
-		# variables accessible in terminal -- get 'schedule/index'
-		# page = HTTParty.get('http://laxpower.com/update15/binboy/XCRAMI.PHP')
+# http://laxpower.com/update15/binboy/XCRAMI.PHP'
 		page = HTTParty.get('http://laxpower.com/update16/binboy/XCRAMI.PHP')
 		parse_page = Nokogiri::HTML(page)
-		schedule_array = []
+#-------------------------------------#
+# available objects in term.
+		schedule_object = Hash.new
+		opponent_array = []
+	# - - - - - - - - - - - - - - #
 		score_array = []
-		data_array = []
+		link_array = []
 		team_data = [Team.all]
 		schedule_hash = Hash.new
-
-		# SCORE-SCRAPER
+#-------------------------------------#
+# SCORE-SCRAPER
 		parse_page.css('.team_table > tbody > tr > td.score').map do |a|
 			score = a.text
 			score_array.push(score)
 		end
 
-
 		parse_page.css('.team_table > tbody > tr > td > a').map do |a|
-			data_array.push(a.attributes["href"].value)
-			post_name = a.text
-			schedule_array.push(post_name)
-			#saving team_name & link to Hash (key, value)
-			schedule_hash["#{post_name}"] = a.attributes["href"].value
+			link_array.push(a.attributes["href"].value)
+			opponent_name = a.text
+			opponent_array.push(opponent_name)
+# Oppenent_name & link to Hash (key, value)
+			schedule_hash["#{opponent_name}"] = a.attributes["href"].value
+
 		end
 
+		temp_opponents = opponent_array			
+		temp_team_links = link_array
+		temp_scores = "score_array"
+		temp_home_away = "home_away_array"
+		temp_results = "result_array"
+#-------------------------------------#
+		schedule_object["opp_teams"] = temp_opponents
+		schedule_object["links"] = temp_team_links
+		schedule_object["scores"] = temp_scores
+		schedule_object["home_away"] = temp_home_away
+		schedule_object["result"] = temp_results
+		
 		Pry.start(binding)
-
 	end
 
 	def save_schedule
+#-------------------------------------#
+			temp_opponents = opponent_array			
+			temp_team_links = link_array
+			temp_scores = "score_array"
+			temp_home_away = "home_away_array"
+			temp_result = "result_array"
+#-------------------------------------#
+			schedule_object["opp_teams"] = temp_opponents
+			schedule_object["links"] = temp_team_link
+			schedule_object["scores"] = temp_scores
+			schedule_object["home_away"] = temp_home_away
+			schedule_object["result"] = result_array
 	end
 
 
