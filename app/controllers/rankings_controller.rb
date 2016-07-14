518979d65d5f29a3a7dcca12e80147f782517a1a
Jonathan Harlan <jjharlan06@gmail.com>
2016-07-14 17:36:45 -0400

    Modify Ranking API Data - client optimization

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,12 +1,20 @@
 class RankingsController < ApplicationController
   def national
     number_of_teams = params[:number_of_teams].to_i
-    ap Team.where(:nat_rank => 1..number_of_teams).order('nat_rank ASC').select(:team_name, :laxid, :nat_rank)
+    rankings = Team.where(:nat_rank => 1..number_of_teams).order('nat_rank ASC').select(:team_name, :laxid, :nat_rank)
+    respond_to do |format|
+      format.json{render json: rankings}
+    end
   end
 
   def state
     state = params[:state]
-    ap Team.where(:state => state).order('nat_rank ASC').select(:team_name, :laxid, :nat_rank, :state)
+    number_of_teams = params[:number_of_teams].to_i
+    rankings = Team.where(:state => state).order('nat_rank ASC').select(:team_name, :laxid, :power_rating, :state, :wins, :losses, :ties).first(number_of_teams)
+    respond_to do |format|
+      format.json{render json: rankings}
+    end
+
   end
 
   def league # NOT ACTIVE
--- app/services/create_team_service.rb
+++ app/services/create_team_service.rb
@@ -1,4 +1,5 @@
 class CreateTeamService
+
 # TO-DO #
 # + @team[:league]
 
