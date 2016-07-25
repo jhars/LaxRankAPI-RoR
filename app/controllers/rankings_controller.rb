52466ef50ad8928adc66a1c094030433a658a545
Jonathan Harlan <jjharlan06@gmail.com>
2016-07-25 00:43:01 -0400

    Add :id to National + State Rankings

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,7 +1,7 @@
 class RankingsController < ApplicationController
   def national
     number_of_teams = params[:number_of_teams].to_i
-    rankings = Team.where(:nat_rank => 1..number_of_teams).order('nat_rank ASC').select(:team_name, :laxid, :nat_rank)
+    rankings = Team.where(:nat_rank => 1..number_of_teams).order('nat_rank ASC').select(:id, :team_name, :laxid, :nat_rank)
     respond_to do |format|
       format.json{render json: rankings}
     end
@@ -10,7 +10,7 @@ class RankingsController < ApplicationController
   def state
     state = params[:state]
     number_of_teams = params[:number_of_teams].to_i
-    rankings = Team.where(:state => state).order('nat_rank ASC').select(:team_name, :laxid, :power_rating, :state, :wins, :losses, :ties).first(number_of_teams)
+    rankings = Team.where(:state => state).order('nat_rank ASC').select(:id, :team_name, :laxid, :power_rating, :state, :wins, :losses, :ties).first(number_of_teams)
     respond_to do |format|
       format.json{render json: rankings}
     end
